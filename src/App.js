e5ab3b18c9de6e3b39c691f005502e8f886abe81
Brayan Leyva <brayanleyvag@gmail.com>
2022-02-27 01:48:11 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { ActionBtn } from './components/ActionBtn';
+import { QuoteAuthor } from './components/QuoteAuthor';
+import { QuoteText } from './components/QuoteText';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>      
+        <QuoteText/>
+        <QuoteAuthor/>
+        <ActionBtn/>
+      </div>
     </div>
   );
 }
---
+++ src/components/QuoteText.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const QuoteText = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
